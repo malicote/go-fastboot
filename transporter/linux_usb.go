dc8ddc3180a0c5d2798460579512d0db525713f4
Troy B <troy.bevis@gmail.com>
2016-04-03 10:50:00 -0700

    More USB work.
    Now can translate to dev node and detect RO.

--- transporter/linux_usb.go
+++ transporter/linux_usb.go
@@ -1,8 +1,12 @@
 package transporter
 
+// TODO: use proper logging (debug, fatal, etc)
+
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
+	"path"
 	"strings"
 	"unicode"
 )
@@ -11,6 +15,8 @@ const (
 	MAX_RETRIES = 5
 	WAIT_FOR_DISCONNECT_TIME = 3
 	MAX_USBFS_BULK_SIZE = (16 * 1024)
+	USB_SYSFS_BASE = "/sys/bus/usb/devices"
+	USB_DEV_BUS_BASE = "/dev/bus/usb"
 )
 
 type UsbHandler struct {
@@ -30,7 +36,29 @@ func NewLinuxUSB(handler UsbHandler) (*LinuxUSB){
 	return &LinuxUSB{ handler }
 }
 
+func convertToDevFSName(dir_name string) (string, error) {
+	bus_num_path := path.Join(USB_SYSFS_BASE, dir_name, "busnum")
+	dev_num_path := path.Join(USB_SYSFS_BASE, dir_name, "devnum")
+
+	busnum, err := ioutil.ReadFile(bus_num_path)
+	if err != nil {
+		return "", err
+	}
+
+	devnum, err := ioutil.ReadFile(dev_num_path)
+	if err != nil {
+		return "", err
+	}
+
+	devpath := path.Join(USB_DEV_BUS_BASE,
+				fmt.Sprintf("%03s", strings.TrimSpace(string(busnum[:]))),
+				fmt.Sprintf("%03s", strings.TrimSpace(string(devnum[:]))))
+
+	return  devpath, nil
+}
+
 func findUsbDevice(base string) (*UsbHandler, error) {
+	// TODO: can this be simplified?
 	is_device := func(path string) bool {
 		if strings.HasPrefix(path, ".") {
 			// Is ./ or ../
@@ -56,14 +84,39 @@ func findUsbDevice(base string) (*UsbHandler, error) {
 		if !is_device(file.Name()) {
 			continue
 		}
-		fmt.Println(file.Name())
+
+		dev_path, err := convertToDevFSName(file.Name())
+		if err != nil {
+			// TODO: verbose printing only
+			fmt.Println(err)
+			continue
+		}
+
+		writeable := true
+		if _, err := os.OpenFile(dev_path, os.O_RDWR, 0); err != nil {
+			// Check if device node is read-only, then we can provide
+			// a helpful error message similar to 'adb devices'
+			// TODO: verbose
+			fmt.Println(err)
+			writeable = false
+			if _, err = os.OpenFile(dev_path, os.O_RDONLY, 0); err != nil {
+				// TODO: verbose
+				fmt.Println("Skipping: ", dev_path, "[", err, "]")
+				continue
+			}
+		}
+
+
+		fmt.Printf("Devfs path %s, writeable: %v\n", dev_path, writeable)
+
 	}
 
 	return new(UsbHandler), nil
 }
 
+// TODO: move this to its own package or figure out way to conditionally compile
 func UsbOpen() {
-	findUsbDevice("/sys/bus/usb/devices")
+	findUsbDevice(USB_SYSFS_BASE)
 }
 
 func (usb *LinuxUSB) Read() ([]byte, error) {
